90cb456eda6e23122adcde2ccf54a2cfc33a0e81
Evgenij Chirkun <evgenij.chirkun@ventionteams.com>
2023-07-24 16:35:15 +0200

    Autocomplete. Mouse actions

---
+++ cypress/e2e/WebdriverUniversity/AutoCompleteHandling.js
@@ -0,0 +1,34 @@
+/// <reference types='Cypress' />
+
+describe("Handling WebdriverUniversity autocomplete", () => {
+  it("Select specific product via autocomplete", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+    cy.get("#autocomplete-textfield").invoke("removeAttr", "target").click();
+
+    cy.get("#myInput").type("A");
+    cy.get("#myInputautocomplete-list > *")
+      .each(($el, index, list) => {
+        const prod = $el.text();
+        const productToSelect = "Avacado";
+
+        if (prod === productToSelect) {
+          $el.trigger('click');
+          cy.get("#submit-button").click();
+          cy.url().should("include", productToSelect);
+        }
+      })
+      .then(() => {
+        cy.get("#myInput").type("G");
+        cy.get("#myInputautocomplete-list > *").each(($el, index, list) => {
+          const prod = $el.text();
+          const productToSelect = "Garlic";
+
+          if (prod === productToSelect) {
+            $el.trigger('click');
+            cy.get("#submit-button").click();
+            cy.url().should("include", productToSelect);
+          }
+        });
+      });
+  });
+});
