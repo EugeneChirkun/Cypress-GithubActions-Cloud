5c034d08ba76bf74107719c181c1db4d89e2fa80
Evgenij Chirkun <evgenij.chirkun@ventionteams.com>
2023-07-26 17:27:11 +0200

    Files upload

---
+++ cypress/e2e/WebdriverUniversity/FileUpload.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+describe("Handling file upload via Webdriveruniversity page", () => {
+  beforeEach(() => {
+    cy.visit("http://webdriveruniversity.com/");
+    cy.get("#file-upload")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+  });
+
+  it("Upload file and verify", () => {
+    cy.get("#myFile").selectFile("cypress/fixtures/laptop.png");
+    cy.get("#submit-button").click;
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("Your file has now been uploaded!");
+    });
+  });
+
+  it("No file upload", () => {
+    cy.get("#submit-button").click;
+
+    cy.on("window:alert", (str) => {
+      expect(str).to.equal("You need to select a file to upload!");
+    });
+  });
+});
