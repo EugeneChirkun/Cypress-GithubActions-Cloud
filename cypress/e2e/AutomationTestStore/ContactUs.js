efa7146c481ba41dc970ec286bd4fb5df5e335d5
Evgenij Chirkun <evgenij.chirkun@ventionteams.com>
2023-07-27 14:25:26 +0200

    Fixtures and aliases

--- cypress/e2e/AutomationTestStore/ContactUs.js
+++ cypress/e2e/AutomationTestStore/ContactUs.js
@@ -1,17 +1,23 @@
 /// <reference types='Cypress' />
 
 describe("Test Contact Us form via Automation Test Store", () => {
+  before(() => {
+    cy.fixture("userDetails").as("user");
+  });
+
   it("Should be able to successfully submit Contact Us form", () => {
     cy.visit("https://automationteststore.com/");
-    // cy.xpath('//a[contains(@href, \'contact\')]').click();
     cy.get("a[href$='contact']")
       .click()
       .then(function (linkText) {
         console.log("Clicked on link with text: " + linkText.text());
       });
-    cy.get("#ContactUsFrm_first_name").type("Johnatan");
-    cy.get("#ContactUsFrm_email").type("john.test.mail@test.com");
-    cy.get("#ContactUsFrm_enquiry").type("Test message");
+
+    cy.get("@user").then((user) => {
+      cy.get("#ContactUsFrm_first_name").type(user.firstName);
+      cy.get("#ContactUsFrm_email").type(user.email);
+      cy.get("#ContactUsFrm_enquiry").type(user.userMessage);
+    });
     cy.get('button[title="Submit"]').click();
     cy.get(".mb40 > :nth-child(3)").should(
       "have.text",
--- cypress/e2e/WebdriverUniversity/ContactUs.js
+++ cypress/e2e/WebdriverUniversity/ContactUs.js
@@ -2,25 +2,28 @@
 
 describe("Test Contact Us form via WebdriverUniversity", () => {
   beforeEach(() => {
+    cy.fixture("example").then((data) => {
+      globalThis.data = data;
+    });
     cy.visit("http://www.webdriveruniversity.com/");
     cy.get("#contact-us").invoke("removeAttr", "target").click();
   });
 
-  it.only("Should be able to successfully submit Contact Us form", () => {
+  it("Should be able to successfully submit Contact Us form", () => {
     cy.document().should("have.property", "charset", "UTF-8");
     cy.title().should("include", "WebDriver");
-    cy.get('[name = "first_name"]').type("John");
-    cy.get('[name="last_name"]').type("Deer");
-    cy.get('[name="email"]').type("johnny.d@testmail.com");
-    cy.get('[name="message"]').type("My test message");
+    cy.get('[name = "first_name"]').type(data.firstName);
+    cy.get('[name="last_name"]').type(data.lastName);
+    cy.get('[name="email"]').type(data.email);
+    cy.get('[name="message"]').type(data.messageText);
     cy.get('[type="submit"]').click();
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
 
   it("Should not be able to successfully submit Contact Us form", () => {
-    cy.get('[name = "first_name"]').type("John");
-    cy.get('[name="last_name"]').type("Deer");
-    cy.get('[name="message"]').type("My test message");
+    cy.get('[name = "first_name"]').type(data.firstName);
+    cy.get('[name="last_name"]').type(data.lastName);
+    cy.get('[name="message"]').type(data.messageText);
     cy.get('[type="submit"]').click();
     cy.get("body").contains("Error:");
   });
