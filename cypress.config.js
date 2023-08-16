5cf5631c763624795c2bf4198ca5c3ff4e885140
Evgenij Chirkun <evgenij.chirkun@ventionteams.com>
2023-08-16 17:51:16 +0200

    Environments, pt. 1

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress/config", `${file}.json`);
+
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: "nk29kz",
@@ -6,7 +19,10 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      // return require("./cypress/plugins/index.js")(on, config);
+      const file = config.env.configFile || "";
+
+      return getConfigurationByFile(file);
     },
     specPattern: "cypress/e2e/**/*.{js, jsx, ts, tsx, feature}",
     experimentalModifyObstructiveThirdPartyCode: true,
